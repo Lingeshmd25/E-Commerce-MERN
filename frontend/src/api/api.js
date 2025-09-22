bdb17df7f7e781c85634bbf8546d71e181d9b3d3
Lingeshwaran D <144088012+Lingeshmd25@users.noreply.github.com>
2025-09-23 00:47:31 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://e-commerce-mern-ccrh.onrender.com/",
+  baseURL: "https://e-commerce-mern-ccrh.onrender.com",
   headers: { "Content-Type": "application/json" }
 });
 
