62fced312a09f509a7120217a08c3fd717c85f48
Lingeshwaran D <144088012+Lingeshmd25@users.noreply.github.com>
2025-09-23 01:08:44 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://e-commerce-mern-ccrh.onrender.com",
+  baseURL: "https://e-commerce-mern-ccrh.onrender.com/api",
   headers: { "Content-Type": "application/json" }
 });
 
-// JWT token interceptor
+// Token attach panna
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
