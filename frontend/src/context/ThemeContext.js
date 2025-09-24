bb7f9dbd157949bb7f6d05061046516e9b3e4d94
Lingeshwaran D <144088012+Lingeshmd25@users.noreply.github.com>
2025-09-24 18:45:01 +0530

    Create ThemeContext.js

---
+++ frontend/src/context/ThemeContext.js
@@ -0,0 +1,22 @@
+import React, { createContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light"); // default
+
+  useEffect(() => {
+    document.body.className = ""; // clear previous
+    document.body.classList.add(`theme-${theme}`);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
